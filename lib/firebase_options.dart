d8f5a2cd909ded471f0bb9ea3b11dec2d5bd615d
TAMIF <tabe7143@gmail.com>
2026-06-18 12:55:28 +0100

    Initial commit - uploading my project

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -14,19 +14,19 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_ANDROID_API_KEY', defaultValue: 'REPLACE_WITH_ANDROID_API_KEY'),
-    appId: String.fromEnvironment('FIREBASE_ANDROID_APP_ID', defaultValue: 'REPLACE_WITH_ANDROID_APP_ID'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID', defaultValue: 'REPLACE_WITH_SENDER_ID'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID', defaultValue: 'REPLACE_WITH_PROJECT_ID'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET', defaultValue: 'REPLACE_WITH_STORAGE_BUCKET'),
+    apiKey: String.fromEnvironment('FIREBASE_ANDROID_API_KEY', defaultValue: 'AIzaSyCa5M7iniZIL7uG0lUNieTdoLo2UlDZd88'),
+    appId: String.fromEnvironment('FIREBASE_ANDROID_APP_ID', defaultValue: '1:431621574250:android:b07811fe4a056552b38413'),
+    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID', defaultValue: 'R431621574250'),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID', defaultValue: 're-kollect-f5c4e'),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET', defaultValue: 're-kollect-f5c4e.firebasestorage.app'),
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_IOS_API_KEY', defaultValue: 'REPLACE_WITH_IOS_API_KEY'),
-    appId: String.fromEnvironment('FIREBASE_IOS_APP_ID', defaultValue: 'REPLACE_WITH_IOS_APP_ID'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID', defaultValue: 'REPLACE_WITH_SENDER_ID'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID', defaultValue: 'REPLACE_WITH_PROJECT_ID'),
+    apiKey: String.fromEnvironment('FIREBASE_IOS_API_KEY', defaultValue: 'AIzaSyDZZk15rVTIIzHFhp9TkcJYVEGkxp9yuNY'),
+    appId: String.fromEnvironment('FIREBASE_IOS_APP_ID', defaultValue: '1:431621574250:ios:5b6254d2df46b73eb38413'),
+    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID', defaultValue: 'R431621574250'),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID', defaultValue: 're-kollect-f5c4e'),
     iosBundleId: 'ai.arena.rekollect',
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET', defaultValue: 'REPLACE_WITH_STORAGE_BUCKET'),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET', defaultValue: 're-kollect-f5c4e.firebasestorage.app'),
   );
 }
